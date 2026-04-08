af263e99ab28f94f4e7b2d5a90fbfbb6ef9e1c8b
ELARBI ALLAM <elarbi.allam.51@gmail.com>
2026-04-08 21:57:15 +0100

    refactor(objc): enhance swizzling utility to safely support metaclass methods

--- Sources/DiagnosticSDKObjC/include/DiagnosticSDKMethodSwizzling.h
+++ Sources/DiagnosticSDKObjC/include/DiagnosticSDKMethodSwizzling.h
@@ -4,8 +4,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DiagnosticSDKMethodSwizzling : NSObject
 
-/// Swaps the implementation of two instance methods for a given class.
-+ (void)swizzleMethod:(SEL)originalSelector withMethod:(SEL)swizzledSelector forClass:(Class)cls;
+/// Swaps the implementation of two class methods (+) safely using the metaclass.
++ (void)swizzleClassMethod:(SEL)originalSelector withMethod:(SEL)swizzledSelector forClass:(Class)cls;
 
 @end
 
